e4f839439679bea665a7c199c4df3b5c88eba6c8
gapizova <kat1502@mail.ru>
2023-09-15 02:02:47 +0500

    TEST: testing function runApp

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -16,12 +17,29 @@ module.exports = {
       template: path.resolve(__dirname, './src/index.html'), // шаблон
       filename: 'index.html', // название выходного файла
     }),
+    new BrowserSyncPlugin(
+      {
+        // browse to http://localhost:3000/ during development
+        host: 'localhost',
+        port: 3000,
+        // proxy the Webpack Dev Server endpoint
+        // (which should be serving on http://localhost:3100/)
+        // through BrowserSync
+        proxy: 'http://localhost:9000/',
+      },
+      // plugin options
+      {
+        // prevent BrowserSync from reloading the page
+        // and let Webpack Dev Server take care of this
+        reload: false,
+      },
+    ),
   ],
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ['styles-loader', 'css-loader'],
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.html$/,
